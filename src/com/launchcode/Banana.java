67d601ba824183d17273116d149e25cec1a58aba
userOskar <oskar.sadlowski@gmail.com>
2017-10-08 20:42:32 +0200

    modifying banana

--- src/com/launchcode/Banana.java
+++ src/com/launchcode/Banana.java
@@ -3,6 +3,8 @@ package com.launchcode;
 public class Banana implements Fruit {
     @Override
     public String getName() {
-        return "BANANA";
+        return "BANANA by Master Branch";
     }
+
+    public void funcionX() {}
 }
